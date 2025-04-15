a225440dc2b06f60b28dea18cf1cfd00f8d7c58b
Prithika <91656490+Prithika496@users.noreply.github.com>
2025-04-15 18:44:03 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+// src/assets/assets.js
+import m2 from './m2.png';
+import m3 from './m3.png';
+import g3 from './g3.png';
+import r1 from './r1.png';
+import girl from './girl.png';
+import boy from './boy.png';
+
+export const assets = {
+    m2,
+    m3,
+    g3,
+    r1,
+    girl,
+    boy
+};
